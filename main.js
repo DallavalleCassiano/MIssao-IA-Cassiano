a8d52a28a5b261c377cf8336fbe8c12e8fdddf7b
Dalla Valle Cassiano <145142163+DallavalleCassiano@users.noreply.github.com>
2025-10-21 11:27:44 -0300

    e

--- main.js
+++ main.js
@@ -79,6 +79,15 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.
+    }
 }
 
 mostraPergunta();
